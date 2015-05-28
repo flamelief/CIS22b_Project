35fe09c9aa45f1b783bb082ffe47b5e15d098258
byost <brcyost@gmail.com>
2015-05-28 16:56:27 -0700

    Report

---
+++ Bookstore/Report.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include "Book.h"
+
+using namespace std;
+
+class Report
+	vector<Book*> books;
+public:
+	int cmpWholesale(Book b1, Book2);
+
+	sort(vector<Book*>)
+{
+public:
+	Report();
+	~Report();
+};
+
