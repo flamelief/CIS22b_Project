9a6a0e5bdd4722cb985bdf886b14e55b3a914d99
mnardin <27.marco@gmail.com>
2015-06-02 15:25:02 -0700

    cashier

--- Bookstore/Cashier.h
+++ Bookstore/Cashier.h
@@ -5,11 +5,17 @@
 
 #include "Book.h"
 
+const double TAX = 0.10;
+
 class Cashier
 {
 private:
 	Price sales;
+	Book book;
 public:
-	
+	double getTax() const { return TAX; }
+	Price getSubtotal() const { return sales; }
+	//Price getTotal() const { return sales + (sales * TAX; }
+	// Subtract book from Inventory database
 };
 #endif
