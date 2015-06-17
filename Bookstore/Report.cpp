fafa7437db415c33f875801e596407792802e1e4
lulu666666 <louise.hsu@comcast.net>
2015-06-17 10:41:53 -0700

    jhjhjhj

--- Bookstore/Report.cpp
+++ Bookstore/Report.cpp
@@ -64,7 +64,7 @@ void Report::printInventory(Sort mode){
 void Report::printInventory() {
 	printInventory(mode);
 }
-
+// sort that takes in type fT
 template <typename fT>
 void Report::sort(fT get) {
 	std::sort(books.begin(), books.end(), 
